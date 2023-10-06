07d6c383a9e1fbd23ca3b03d61972e04ee8556ab
Wilma Wiker <wikerwilma@gmail.com>
2023-10-06 08:55:11 +0200

    installed sass and react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./index.scss";
+import Home from "./pages/Home.js";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <>
+      <div className="landing-page">
+        <h1>Git game!</h1>
+      </div>
+    </>
+  );
+}
+
+export default Home;
