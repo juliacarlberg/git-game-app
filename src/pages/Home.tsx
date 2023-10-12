ef7921a7fd27943566956ed7ac5b2da7adb02c74
Julia Carlberg <juliacarlbrg@gmail.com>
2023-10-12 23:41:46 +0200

    add buttons for continue or start new game, home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,23 @@
 import { Header } from "../components/Header";
+import { ButtonStyled } from "../styled components/ButtonStyled";
+import { HomeWrapper, WrapperStyled } from "../styled components/WrapperStyled";
 
 function Home() {
   return (
     <>
       <Header></Header>
-      <div className="landing-page">
-        <h1>Git game!</h1>
-      </div>
+      <WrapperStyled>
+        <HomeWrapper>
+          <i className="fa-brands fa-github"></i>
+          <h1> GIT GAME</h1>
+          <p>
+            Vill du lära dig Git? Spela vårat spel och lär dig hur git fungerar
+            samt terminalen och dess kommandon.
+          </p>
+          <ButtonStyled>Fortsätt spela</ButtonStyled>
+          <ButtonStyled>Starta nytt spel</ButtonStyled>
+        </HomeWrapper>
+      </WrapperStyled>
     </>
   );
 }
---
+++ src/styled components/ButtonStyled.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const ButtonStyled = styled.button`
+  background-color: #c06ca2;
+  color: white;
+  padding: 3%;
+  margin: 2%;
+  font-size: 16px;
+  font-weight: bold;
+  text-transform: uppercase;
+  border: 5px solid #ffc0e9;
+  border-radius: 30px;
+  cursor: pointer;
+  :hover {
+    background-color: #ffc0e9;
+    border: 5px solid #c06ca2;
+  }
+`;
