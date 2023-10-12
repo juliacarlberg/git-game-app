1e87924ec639c585993da13e810297127a6199a8
Wilma Wiker <wikerwilma@gmail.com>
2023-10-12 14:17:22 +0200

    (style):added icons for level one cards

--- src/components/GitCard.tsx
+++ src/components/GitCard.tsx
@@ -1,9 +1,12 @@
+// @ts-ignore
+import FeatherIcon from "feather-icons-react";
+
 export const GitCard = (props: IGitCardProps) => {
   return (
     <>
       <div className="card">
         <h1>{props.title}</h1>
-        <img src={props.imageUrl}></img>
+        <FeatherIcon icon={props.icon} size="75" color="black" />
         <p>{props.desc}</p>
       </div>
     </>
