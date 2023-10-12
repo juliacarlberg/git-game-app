cade04de814edd0e676597ee8dafe173bda14113
Wilma Wiker <wikerwilma@gmail.com>
2023-10-12 13:22:50 +0200

    (refactor):made GitCard dynamic for all cards

---
+++ src/components/GitCard.tsx
@@ -0,0 +1,17 @@
+interface IGitCardProps {
+  title: string;
+  imageUrl: string;
+  desc: string;
+}
+
+export const GitCard = (props: IGitCardProps) => {
+  return (
+    <>
+      <div className="card">
+        <h1>{props.title}</h1>
+        <img src={props.imageUrl}></img>
+        <p>{props.desc}</p>
+      </div>
+    </>
+  );
+};
